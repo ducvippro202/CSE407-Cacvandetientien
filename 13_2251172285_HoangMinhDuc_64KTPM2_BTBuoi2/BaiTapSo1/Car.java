d922395533f2fc7da8768f32fe5cd9b5d6605f45
ducvippro202 <167794424+ducvippro202@users.noreply.github.com>
2025-05-07 08:01:04 +0700

    Delete 13_2251172285_HoangMinhDuc_64KTPM2_BTBuoi2/BaiTapSo1 directory

--- 13_2251172285_HoangMinhDuc_64KTPM2_BTBuoi2/BaiTapSo1/Car.java
+++
@@ -1,6 +0,0 @@
-
-public class Car extends Vehicle {
-	public void Honk() {
-		System.out.println("Beep Beep");
-	}
-}
--- 13_2251172285_HoangMinhDuc_64KTPM2_BTBuoi2/BaiTapSo1/Motorbike.java
+++
@@ -1,7 +0,0 @@
-
-public class Motorbike extends Vehicle {
-	public void RevEngine() {
-		System.out.println("reverse");
-	}
-	
-}
