0c2d44a670f5f3c441e51677f619e4f19a2cf7cc
ducvippro202 <167794424+ducvippro202@users.noreply.github.com>
2025-05-07 08:02:06 +0700

    Add files via upload

---
+++ 13_2251172285_HoangMinhDuc_64KTPM2_BTBuoi1/BaiTapSo1/Vehicle.java
@@ -0,0 +1,29 @@
+
+public class Vehicle {
+	protected String Brand;
+	protected float MaxSpeed;
+	
+	public Vehicle()
+	{
+		
+	}
+
+	public String getBrand() {
+		return Brand;
+	}
+
+	public void setBrand(String brand) {
+		Brand = brand;
+	}
+
+	public float getMaxSpeed() {
+		return MaxSpeed;
+	}
+
+	public void setMaxSpeed(float maxSpeed) {
+		MaxSpeed = maxSpeed;
+	}
+	
+	
+
+}
